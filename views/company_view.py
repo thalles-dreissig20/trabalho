2d57a11591f26df23632b8716f1d35b4340f3070
Thalles Rafael Dreissig <thallesr.dreissig@gmail.com>
2025-06-29 19:55:58 -0300

    Telas companies

--- views/company_view.py
+++ views/company_view.py
@@ -3,6 +3,7 @@ from exceptions.index import MenuOptionError
 from tabulate import tabulate
 
 from views.AbstractCodeView import AbstractCodeView
+import dearpygui.dearpygui as dpg
 
 class CompanyView(AbstractCodeView): 
     def main_menu(self):
@@ -27,20 +28,77 @@ class CompanyView(AbstractCodeView):
 
     
     def form(self):
-        cnpj = input("Digite o CNPJ: ")
-        nome = input("Digite a razão social: ")
-        return cnpj, nome
+        self.input_cnpj = ""
+        self.input_name = ""
+
+        def submit_callback():
+            self.input_cnpj = dpg.get_value("cnpj_input")
+            self.input_name = dpg.get_value("name_input")
+            dpg.stop_dearpygui()
+
+        dpg.create_context()
+        with dpg.window(label="Cadastro de Empresa", width=400, height=250):
+            dpg.add_input_text(label="CNPJ", tag="cnpj_input")
+            dpg.add_input_text(label="Razão Social", tag="name_input")
+            dpg.add_button(label="Confirmar", callback=submit_callback)
+
+        dpg.create_viewport(title='Formulário Empresa', width=400, height=250)
+        dpg.setup_dearpygui()
+        dpg.show_viewport()
+        dpg.start_dearpygui()
+        dpg.destroy_context()
+
+        return self.input_cnpj, self.input_name
+
+
+    def get_code(self, message="Digite o código:"):
+        code_value = {"value": None}
+
+        def submit():
+            try:
+                code_value["value"] = int(dpg.get_value("code_input"))
+                dpg.stop_dearpygui()
+            except ValueError:
+                dpg.set_value("error_text", "❌ Digite um número válido.")
+
+        dpg.create_context()
+        with dpg.window(label="Código da Empresa", width=400, height=200):
+            dpg.add_text(message)
+            dpg.add_input_text(label="Código", tag="code_input")
+            dpg.add_button(label="Confirmar", callback=submit)
+            dpg.add_text("", tag="error_text", color=(255, 0, 0))
+
+        dpg.create_viewport(title='Código da Empresa', width=400, height=200)
+        dpg.setup_dearpygui()
+        dpg.show_viewport()
+        dpg.start_dearpygui()
+        dpg.destroy_context()
+
+        return code_value["value"]
 
 
     def show_companies(self, companies):
-        if not companies:
-            print("Nenhuma empresa cadastrada.")
-            return
-        print('\n\n')
-        headers = ["Código", "Razão Social", "CNPJ", "Agência Pública Associada"]
-        table_data = [
-            [company.code, company.social_reason, company.cnpj, company.public_agency.social_reason]
-            for company in companies
-        ]
+        dpg.create_context()
+        with dpg.window(label="Empresas Cadastradas", width=800, height=500):
+            if not companies:
+                dpg.add_text("Nenhuma empresa cadastrada.")
+            else:
+                dpg.add_text("Lista de Empresas Cadastradas")
+                with dpg.table(header_row=True, resizable=True, policy=dpg.mvTable_SizingStretchProp, row_background=True):
+                    dpg.add_table_column(label="Código")
+                    dpg.add_table_column(label="Razão Social")
+                    dpg.add_table_column(label="CNPJ")
+                    dpg.add_table_column(label="Agência Pública")
 
-        print(tabulate(table_data, headers=headers, tablefmt="fancy_grid", stralign="left"))
+                    for company in companies:
+                        with dpg.table_row():
+                            dpg.add_text(str(company.code))
+                            dpg.add_text(company.social_reason)
+                            dpg.add_text(company.cnpj)
+                            dpg.add_text(company.public_agency.social_reason)
+
+        dpg.create_viewport(title='Empresas', width=800, height=500)
+        dpg.setup_dearpygui()
+        dpg.show_viewport()
+        dpg.start_dearpygui()
+        dpg.destroy_context()
